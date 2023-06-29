de636ab79aebfea8c151bd9a32536d42a6deccce
nandishTech01 <nadishbhuva5@gmail.com>
2023-06-29 08:57:25 +0530

    additional changes ofmorning 29-6

--- ReactBank/src/actions/projectActions.jsx
+++ ReactBank/src/actions/projectActions.jsx
@@ -1,16 +1,22 @@
 import axios from 'axios';
 import { GET_ERRORS } from './types';
+import { useNavigate } from 'react-router-dom';
 
-export const createWallet = (newWallet, navigate) => async (dispatch) => {
+export const createWallet = (newWallet, history) => async dispatch => {
   try {
-    const res = await axios.post('http://localhost:8081/wallet', newWallet);
-    navigate('/dashboard');
-  } catch (err) {
-    if (err.response && err.response.data) {
-      dispatch({ type: GET_ERRORS, payload: err.response.data });
+    const response = await axios.post('http://localhost:8081/wallet', newWallet);
+    console.log('Response:', response); // Check the response
+    if (response) {
+      // window.location.replace('/dashboard');
+      const navigate = useNavigate();
+      navigate('/dashboard');
+
     } else {
-      console.log('Error:', err);
-      // Handle other error scenarios or show a generic error message.
+      console.log('Invalid history object:', history);
     }
+  } catch (err) {
+    console.log('Error:', err); // Check the error
+    console.log('Error response:', err.response); // Check the error response
+    dispatch({ type: GET_ERRORS, payload: err.response ? err.response.data : 'Unknown error occurred' });
   }
 };
